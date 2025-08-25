11c7846105ac9c144ae70c5fbc01adfd892336d3
Dennis Möhlmann <dmo479@gmail.com>
2025-08-25 04:08:22 +0200

    refactor circuit management into separate class

--- logic/main.js
+++ logic/main.js
@@ -3,66 +3,23 @@
 // Create grid and toolbar
 let mainGrid = new Grid(document.querySelector('#content'));
 let toolbar = new Toolbar(mainGrid, document.querySelector('#toolbar'));
-
-let circuits = [ ];
-let currentCircuit = 0;
-
-function circuitName(name) {
-    return name ?? 'New circuit #' + (circuits.length + 1);
-};
-
-circuits.push({ label: circuitName(), data: [] });
+let circuits = new Circuits(mainGrid);
 
 // Add file operations to toolbar
 let [ fileMenu, fileMenuState ] = toolbar.createMenuButton('File', 'File operations menu. <i>LMB</i> Open menu.');
 
-function saveGridCircuit() {
-    if (currentCircuit !== -1) {
-        circuits[currentCircuit].data = mainGrid.serialize();
-    }
-}
-function switchGridCircuit(newCircuit) {
-    mainGrid.clear();
-    mainGrid.unserialize(circuits[newCircuit].data);
-    currentCircuit = newCircuit;
-}
-function pruneEmptyCircuits() {
-    for (let i = circuits.length - 1; i >= 0; --i) {
-        if (circuits[i].data.length <= 1 && circuits[i].data.filter((i) => i['_']['c'] !== 'Grid').length === 0) {
-            circuits.splice(i, 1);
-            if (currentCircuit === i) {
-                currentCircuit = -1;
-            }
-        }
-    }
-}
-
 fileMenu.createActionButton('New', 'Close all open circuits', async () => {
     fileMenuState(false);
-    circuits = [ ];
-    circuits.push({ label: circuitName(), data: [] });
-    currentCircuit = 0;
+    circuits.clear();
     updateCircuitMenu();
-    mainGrid.clear();
-    mainGrid.render();
 });
 fileMenu.createActionButton('Open...', 'Load circuit from disk', async () => {
     fileMenuState(false);
     let [ fileHandle ] = await window.showOpenFilePicker();
     const file = await fileHandle.getFile();
     const content = JSON.parse(await file.text());
-    saveGridCircuit();
-    pruneEmptyCircuits();
-    const newCircuitIndex = circuits.length;
-    if (typeof content.version === 'undefined') {
-        circuits.push({ label: circuitName(file.name.replace(/\.stc$/, '')), data: content });
-        switchGridCircuit(newCircuitIndex);
-    } else if (content.circuits.length > 0) {
-        circuits.push(...content.circuits);
-        switchGridCircuit(newCircuitIndex);
-    }
+    circuits.unserialize(content, true, file.name);
     updateCircuitMenu();
-    mainGrid.render();
 });
 fileMenu.createActionButton('Save as...', 'Save circuit to disk', async () => {
     fileMenuState(false);
@@ -78,8 +35,7 @@ fileMenu.createActionButton('Save as...', 'Save circuit to disk', async () => {
     };
     const handle = await window.showSaveFilePicker(options);
     const writable = await handle.createWritable();
-    saveGridCircuit();
-    await writable.write(JSON.stringify({ version: 1, circuits }));
+    await writable.write(JSON.stringify(circuits.serialize()));
     await writable.close();
 });
 
@@ -87,35 +43,22 @@ fileMenu.createActionButton('Save as...', 'Save circuit to disk', async () => {
 
 let [ circuitMenu, circuitMenuState ] = toolbar.createMenuButton('Circuit', 'Circuit management menu. <i>LMB</i> Open menu.');
 
-function selectCircuit(newCircuitIndex) {
-    circuitMenuState(false);
-    saveGridCircuit();
-    switchGridCircuit(newCircuitIndex);
-    mainGrid.render();
-}
-
-function createCircuit() {
-    circuitMenuState(false);
-    saveGridCircuit();
-    mainGrid.clear();
-    currentCircuit = circuits.length;
-    const name = prompt('Circuit name', circuitName());
-    circuits.push({ label: name, data: [] });
-    updateCircuitMenu();
-    mainGrid.render();
-}
-
 function updateCircuitMenu() {
     circuitMenu.clear();
-    circuitMenu.createActionButton('Create...', 'Create a new circuit.', createCircuit);
-
-    for (let [ index, { label } ] of circuits.entries()) {
-        circuitMenu.createActionButton(label, 'Switch grid to circuit "' + label + '"', () => selectCircuit(index));
+    circuitMenu.createActionButton('Create...', 'Create a new circuit.', () => {
+        circuitMenuState(false);
+        circuits.create();
+        updateCircuitMenu();
+    });
+    for (let [ index, label ] of circuits.list().entries()) {
+        circuitMenu.createActionButton(label, 'Switch grid to circuit "' + label + '"', () => {
+            circuitMenuState(false);
+            circuits.select(index);
+        });
     }
 }
 
 updateCircuitMenu();
-
 
 // Add standard components to toolbar
 toolbar.createComponentButton('Port ·', 'Component IO pin. <i>LMB</i>: Drag to move onto grid.', (grid, x, y) => new Port(grid, x, y, 'right'));
---
+++ logic/ui/circuits.js
@@ -0,0 +1,99 @@
+// Handles loading/saving/selecting circuits and keeping the grid updated.
+class Circuits {
+
+    #circuits = [];
+    #currentCircuit = 0;
+    #grid;
+
+    constructor(grid) {
+        this.#circuits.push({ label: this.#generateName(), data: [] });
+        this.#grid = grid;
+    }
+
+    // Returns a list of loaded circuits.
+    list() {
+        return this.#circuits.map((c) => c.label);
+    }
+
+    // Clear all circuits and create a new empty circuit (always need one for the grid).
+    clear() {
+        this.#circuits = [ ];
+        this.#circuits.push({ label: this.#generateName(), data: [] });
+        this.#currentCircuit = 0;
+        this.#grid.clear();
+        this.#grid.render();
+    }
+
+    // Selects a circuit by index.
+    select(newCircuitIndex) {
+        this.#saveGrid();
+        this.#setGrid(newCircuitIndex);
+        this.#grid.render();
+    }
+
+    // Creates a new circuit.
+    create() {
+        this.#saveGrid();
+        this.#grid.clear();
+        this.#currentCircuit = this.#circuits.length;
+        // TODO: need proper input component
+        const name = prompt('Circuit name', this.#generateName()); // TBD: maybe not have the prompt here? but need public generateName then
+        this.#circuits.push({ label: name, data: [] });
+        this.#grid.render();
+    }
+
+    // Serializes loaded circuits for saving to file.
+    serialize() {
+        this.#saveGrid();
+        return { version: 1, circuits: this.#circuits };
+    }
+
+    // Unserializes circuits from file.
+    unserialize(content, append, filename) { // TODO: option not to append
+        this.#saveGrid();
+        this.#pruneEmpty();
+        const newCircuitIndex = this.#circuits.length;
+        if (typeof content.version === 'undefined') {
+            // legacy: single circuit in file, name it from the filename, then switch to it
+            this.#circuits.push({ label: this.#generateName(filename).replace(/\.stc$/, ''), data: content });
+            this.#setGrid(newCircuitIndex);
+        } else if (content.circuits.length > 0) {
+            // multiple circuits, switch to first of them
+            this.#circuits.push(...content.circuits);
+            this.#setGrid(newCircuitIndex);
+        }
+        this.#grid.render();
+    }
+
+    // Returns a generated name if the given name is empty.
+    #generateName(name) {
+        return name || 'New circuit #' + (this.#circuits.length + 1);
+    }
+
+    // Saves current grid contents to current circuit.
+    #saveGrid() {
+        if (this.#currentCircuit !== -1) {
+            this.#circuits[this.#currentCircuit].data = this.#grid.serialize();
+        }
+    }
+
+    // Replaces the current grid contents with the given circuit. Does NOT save current contents.
+    #setGrid(newCircuit) {
+        this.#grid.clear();
+        this.#grid.unserialize(this.#circuits[newCircuit].data);
+        this.#currentCircuit = newCircuit;
+    }
+
+    // Prunes empty circuits (app starts with one empty circuit and loading appends by default, so the empty circuit should be pruned).
+    #pruneEmpty() {
+        for (let i = this.#circuits.length - 1; i >= 0; --i) {
+            // TODO: don't prune user created empty circuits
+            if (this.#circuits[i].data.length <= 1 && this.#circuits[i].data.filter((i) => i['_']['c'] !== 'Grid').length === 0) {
+                this.#circuits.splice(i, 1);
+                if (this.#currentCircuit === i) {
+                    this.#currentCircuit = -1;
+                }
+            }
+        }
+    }
+}
