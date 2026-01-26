106c033e130b9f302c361cf68898e35e92fc7289
Dennis Möhlmann <dmo479@gmail.com>
2026-01-26 20:51:01 +0100

    implement additional text properties  for color and rotation

--- logic/grid/textlabel.js
+++ logic/grid/textlabel.js
@@ -5,41 +5,47 @@ class TextLabel extends GridItem {
 
     static #EDIT_DIALOG = [
         { name: 'text', label: 'Text', type: 'string', check: (v, f) => v.trim().length > 0 },
+        { name: 'fontSize', label: 'Font size', type: 'select', options: { "small": "Small", "medium": "Medium", "large": "Large" } },
+        { name: 'color', label: 'Color', type: 'select', options: { "-": "White", "c0": "Mint", "c1": "Green", "c2": "Yellow", "c3": "Orange", "c4": "Red", "c5": "Ruby", "c6": "Magenta", "c7": "Purple", "c8": "Blue", "c9": "Turquoise" } },
         { name: 'maxLength', label: 'Max. width', type: 'int', check: (v, f) => { const p = Number.parseInt(v); return isFinite(p) && p >= Grid.SPACING; } },
-        //...Component.EDIT_DIALOG,
+        ...Component.EDIT_DIALOG,
     ];
 
     #element;
     #inner;
     #dropPreview;
+    #fontSize;
     #color;
     #text;
     #rotation;
 
-    constructor(app, x, y, rotation, maxLength = 180, text = 'Placeholder text. Press e to edit.', color = null) {
-        assert.string(text);
+    constructor(app, x, y, rotation, maxLength = 180, text = 'Placeholder text. Press e to edit.', fontSize = 'small', color = null) {
+        assert.integer(rotation);
         assert.integer(maxLength);
+        assert.string(text);
+        assert.string(fontSize);
         assert.integer(color, true);
         super(app, x, y);
         this.width = maxLength;
         this.height = Grid.SPACING;
         this.#text = text;
-        this.#color = color ?? null;
-        this.#rotation = rotation ?? null;
+        this.#fontSize = fontSize;
+        this.#color = color;
+        this.#rotation = rotation & 3;
     }
 
     // Serializes the object for writing to disk.
     serialize() {
         return {
             ...super.serialize(),
-            _: { c: this.constructor.name, a: [ this.x, this.y, this.#rotation, this.width, this.#text, this.#color ]},
+            _: { c: this.constructor.name, a: [ this.x, this.y, this.#rotation, this.width, this.#text, this.#fontSize, this.#color ]},
         };
     }
 
     // Link wire to a grid, enabling it to be rendered.
     link(grid) {
         super.link(grid);
-        this.#element = element(null, 'div', 'text text-' + this.#rotation);
+        this.#element = element(null, 'div', 'text');
         this.#inner = element(this.#element, 'span', 'inner'); // not assigning contents here since we don't want to use innerHTML
         this.#inner.innerText = this.#text;
         this.setHoverMessage(this.#inner, () => `Text element. <i>E</i> Edit, ${Component.HOTKEYS}.`, { type: 'hover' });
@@ -69,12 +75,12 @@ class TextLabel extends GridItem {
         this.#element.classList.toggle('selected', status);
     }
 
-    // Return wire color.
+    // Return text color.
     get color() {
         return this.#color;
     }
 
-    // Set wire color.
+    // Set text color.
     set color(value) {
         assert.integer(value, true);
         this.dirty ||= this.#color !== value;
@@ -85,7 +91,7 @@ class TextLabel extends GridItem {
     onHotkey(key, what) {
         if (key === 'r' && what.type === 'hover') {
             // rotate component with R while mouse is hovering
-            this.rotation += 1;
+            this.#rotation = (this.#rotation + 1) & 3;
             this.#element.classList.add('component-rotate-animation');
             setTimeout(() => {
                 // queue class removal for next render call to avoid brief flickering
@@ -149,10 +155,12 @@ class TextLabel extends GridItem {
 
     // Handle edit hotkey.
     async onEdit() {
-        const config = await dialog("Configure text element", TextLabel.#EDIT_DIALOG, { text: this.#text, maxLength: this.width, rotation: this.rotation });
+        const config = await dialog("Configure text element", TextLabel.#EDIT_DIALOG, { text: this.#text, maxLength: this.width, fontSize: this.#fontSize, color: this.#color === null ? '-' : 'c' + this.#color, rotation: this.#rotation });
         if (config) {
             this.#text = config.text;
             this.width = config.maxLength;
+            this.#fontSize = config.fontSize;
+            this.#color = config.color === '-' ? null : Number.parseInt(config.color.slice(1));
             this.#rotation = config.rotation;
             this.redraw();
         }
@@ -169,8 +177,18 @@ class TextLabel extends GridItem {
         this.#element.style.maxWidth = v.width + "px";
         this.#element.style.width = 'auto';
         this.#element.style.height = 'auto';
+        this.#element.setAttribute('data-text-rotation', this.#rotation);
+
         if (this.dirty) {
             this.#inner.innerText = this.#text;
+            if (this.#color !== null) {
+                this.#inner.setAttribute('data-net-color', '' + this.#color);
+            } else {
+                this.#inner.removeAttribute('data-net-color');
+            }
+            for (const size of [ 'small', 'medium', 'large' ]) {
+                this.#element.classList.toggle(`size-${size}`, this.#fontSize === size);
+            }
         }
         return true;
     }
